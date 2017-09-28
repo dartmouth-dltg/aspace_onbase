ae0d7e9cf237435992e66166c7ab8480564e7da6
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2017-09-28 13:19:56 -0400

    update plugin_init

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,5 +1,4 @@
-my_routes = [File.join(File.dirname(__FILE__), "routes.rb")]
-ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
+ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
 require_relative "../lib/document_keyword_definitions"
 require_relative "../lib/file_buffer"
